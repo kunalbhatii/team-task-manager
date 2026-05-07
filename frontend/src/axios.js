b2da8f3efbf1f64536a10b881575bb62d3513060
Kunal Bhati <kunalbhati7071@gmail.com>
2026-05-07 12:52:37 +0530

    frontend connected to render

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://team-task-manager-wnyk.onrender.com/api",
 });
 
 export default API;
