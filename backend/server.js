f6a44bf220143d2533e9fd8858eae8c6b80e979e
Kunal Bhati <kunalbhati7071@gmail.com>
2026-05-07 12:42:39 +0530

    fixed backend

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,25 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRoutes = require("./routes/authRoutes");
-const taskRoutes = require("./routes/taskRoutes");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const taskRoutes = require("./routes/taskRoutes");
+app.use("/api/tasks", taskRoutes);
+
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-    app.listen(5000, () => {
-      console.log("Server running on 5000");
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
 
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
